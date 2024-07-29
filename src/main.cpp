8b213745a574757030d03e754f1ce0bba6aefee3
Timothée Lafay <87896378+Tlafay1@users.noreply.github.com>
2024-07-29 11:18:09 +0000

    check lock file exists

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,11 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
 {
     if (getuid())
         std::cout << "This program must be run as root\nQuitting !\n";
+
+    if (access("/var/lock/matt_daemon.lock", F_OK) != -1)
+    {
+        std::cout << "Daemon already running\nQuitting !\n";
+        return 1;
+    }
     return 0;
 }
