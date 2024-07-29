f4bb4423808da34125dbc9e59e306462e4a52119
Timothée Lafay <87896378+Tlafay1@users.noreply.github.com>
2024-07-29 09:42:54 +0000

    Force to run as root

--- includes/matt_daemon.hpp
+++ includes/matt_daemon.hpp
@@ -1,4 +1,7 @@
 #ifndef MATT_DAEMON_HPP
 #define MATT_DAEMON_HPP
 
+#include <unistd.h>
+#include <iostream>
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
+#include "Matt_daemon.hpp"
+
 int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
 {
+    if (getuid())
+        std::cout << "This program must be run as root\nQuitting !\n";
     return 0;
 }
